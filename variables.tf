d5faf71b770cb85b952f5070ea3486b10518b85a
tfdc-student <101262372+tfdc-student@users.noreply.github.com>
2023-09-20 14:06:17 +0200

    Added variables.tf to lab 6

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "pod" {
+  type = string
+}
+
+variable "nd_username" {
+  type = string
+}
+
+variable "nd_password" {
+  type = string
+}
+
+variable "nd_url" {
+  type = string
+}
+
+variable "platform" {
+  type = string
+}
+
+variable "sites" {
+  type    = list(string)
+  default = ["ACI-01", "ACI-02"]
+}
